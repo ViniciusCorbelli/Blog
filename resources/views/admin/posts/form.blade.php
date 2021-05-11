2b78da9cbee535320b2c73dd053f40d88f518add
Vinicius Corbelli <v.corbelli71@gmail.com>
2021-05-11 18:23:57 -0300

    Trocando o textarea do CRUD posts

--- resources/views/admin/posts/form.blade.php
+++ resources/views/admin/posts/form.blade.php
@@ -20,7 +20,7 @@
 </div>
 <div class="form-group">
     <label for="message" class="required">Mensagem </label>
-    <textarea name="message" class="textarea" style="width: 730px; height: 200px"
+    <textarea name="message" class="textarea" style="width: 730px; height: 200px; max-width: 100%"
         value="{{ old('message', $post->message) }}"> {{ $post->message }} </textarea>
 </div>
 
@@ -31,26 +31,19 @@
     <script src="https://jhollingworth.github.io/bootstrap-wysihtml5//lib/js/bootstrap.min.js"></script>
     <script src="https://jhollingworth.github.io/bootstrap-wysihtml5//src/bootstrap-wysihtml5.js"></script>
     <script>
-        $(function() {
-            //bootstrap WYSIHTML5 - text editor
-            $('.textarea').wysihtml5({
-                locale: "pt-BR",
-                toolbar: {
-                    "font-styles": true, // Font styling, e.g. h1, h2, etc.
-                    "emphasis": true, // Italics, bold, etc.
-                    "lists": true, // (Un)ordered lists, e.g. Bullets, Numbers.
-                    "html": false, // Button which allows you to edit the generated HTML.
-                    "link": false, // Button to insert a link.
-                    "image": true, // Button to insert an image.
-                    "color": true, // Button to change color of font
-                    "blockquote": true, // Blockquote,
-                    parser: function(html) {
-                        return html.text();
-                    }
-                }
-            });
+        $('textarea').wysihtml5({
+            "style": true,
+            "font-styles": false,
+            "emphasis": true,
+            "lists": true,
+            "html": false,
+            "link": true,
+            "image": true,
+            "color": true,
+            parser: function(html) {
+                return html;
+            }
         });
-
     </script>
 
 @endpush
