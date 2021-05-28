5bc67290d714b3db4605eb999b507cd40192db2c
Vinicius Corbelli <v.corbelli71@gmail.com>
2021-05-28 18:59:14 -0300

    Filtro por data infinito

--- resources/views/blog/date/index.blade.php
+++ resources/views/blog/date/index.blade.php
@@ -14,36 +14,31 @@
                 <div class="card-post categories">
                     <h1>Datas</h1>
                     @php
+                        $qnt = 24;
                         $months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+                        $month = strtotime(date('Y-m'));
                     @endphp
 
-                    @for ($i = 0; $i < 12; $i++)
-                        @php
-                            $month = (date('m') + $i) % 12 + 1;
-                            $year = 12 - date('m') + $i >= 2 * (12 - date('m')) ? date('Y') : date('Y') - 1;
-                        @endphp
+                    @for ($i = 0; $i < $qnt; $i++)
                         <hr>
                         <div class="row">
                             <div class="col-8">
                                 <h6> <Strong>
-                                        <a href="{{ route('blog.date.view', [$month, $year]) }}">
-                                            {{ $months[$month-1] }} de {{ $year }}</a>
+                                        <a href="{{ route('blog.date.view', [date('m', $month), date('Y', $month)]) }}">
+                                            {{ $months[date('m', $month) - 1] }} de {{ date('Y', $month) }}</a>
                                     </Strong> </h6>
                             </div>
                             <div class="col-4 text-right">
-                                @php
-                                    if ($month < 10) {
-                                        $month = 0 . $month;
-                                    }
-                                @endphp
                                 <p> {{ count(
-    App\Post::whereMonth('created_at', $month)->whereYear('created_at', $year)->get(),
+    App\Post::whereMonth('created_at', date('m', $month))->whereYear('created_at', date('Y', $month))->get(),
 ) }}
                                     postagens</p>
                             </div>
                         </div>
+                        @php
+                            $month = strtotime('-1 month', $month);
+                        @endphp
                     @endfor
-
                 </div>
             </div>
             <div class="col-sm-4">
